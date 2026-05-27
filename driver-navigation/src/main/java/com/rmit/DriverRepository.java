faa8975f9b1b20e168e61c3f79a5cead346ab731
Curtfoglaw <curtlawrence1@gmail.com>
2026-05-27 20:05:35 +1000

    Pushing with new dependencies and some basic skeleton code for each of my classes

---
+++ driver-navigation/src/main/java/com/rmit/DriverRepository.java
@@ -0,0 +1,16 @@
+package com.rmit;
+
+public class DriverRepository {
+    public void Add() {
+        return;
+    }
+    public void Update() {
+        return;
+    }
+    public void Retrieve() {
+        return;
+    }
+    public int Count() {
+        return 0;
+    }
+}
