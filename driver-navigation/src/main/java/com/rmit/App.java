b42caca90d978d3cd9f9b899574e8a841cfed5cb
Curtfoglaw <curtlawrence1@gmail.com>
2026-05-28 12:06:10 +1000

    No major changes just more porgram functionality testing for the new 'Retrieve' method

--- driver-navigation/src/main/java/com/rmit/App.java
+++ driver-navigation/src/main/java/com/rmit/App.java
@@ -9,16 +9,12 @@ public class App
 {
     public static void main(String[] args)
     {
-        // Creating a sort of "pseudo-memory" that can be used to access and store drivers
-        // Drivers from this list can then be added to the .txt file, details can be edited etc
-
-        ArrayList<Driver> drivers; 
-        drivers = new ArrayList<>();
 
         Driver driver = new Driver("34@#1234AA", "John", 10, "Heavy", "64|stink|melbourne|victoria|Australia", "10-10-1990");
         DriverRepository repo = new DriverRepository();
         repo.Add(driver);
         repo.Retrieve("34@#1234AA");
+        repo.Count();
     }
 
 
