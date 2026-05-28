058a634a51f38ef82899f4889cc67441c7399133
Curtfoglaw <curtlawrence1@gmail.com>
2026-05-28 11:48:49 +1000

    Added 'Retrieve()' functionality

--- driver-navigation/src/main/java/com/rmit/DriverRepository.java
+++ driver-navigation/src/main/java/com/rmit/DriverRepository.java
@@ -1,11 +1,15 @@
 package com.rmit;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class DriverRepository {
 
     FileWriter writer;
-    
+    File myFile;
+
     public void Add(Driver driver) {
         try {
             writer = new FileWriter("driver-navigation\\src\\DriverStorage.txt", true);
@@ -17,11 +21,35 @@ public class DriverRepository {
             e.printStackTrace();
         }
     }
+
     public void Update() {
         return;
     }
-    public void Retrieve() {
-        return;
+    public String Retrieve(String driverID) {
+        try {
+            myFile = new File("driver-navigation\\src\\DriverStorage.txt");
+            Scanner scnr = new Scanner(myFile);
+
+            while (scnr.hasNextLine()) {
+
+                String line = scnr.nextLine();
+                String[] driverInfo = line.split(", ");
+
+                if (driverInfo[0].equals(driverID)) {
+                    scnr.close();
+                    System.out.println("Driver found: \n");
+                    System.out.println(line);
+                    return line;
+                }
+
+            }
+            scnr.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Driver not found");
+        return null;
+
     }
     public int Count() {
         return 0;
