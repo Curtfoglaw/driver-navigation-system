9317913bba7c2c8f0aafdc14a420c1c3a9d48119
Curtfoglaw <curtlawrence1@gmail.com>
2026-05-28 11:47:54 +1000

    added more program functionality testing

--- driver-navigation/src/main/java/com/rmit/App.java
+++ driver-navigation/src/main/java/com/rmit/App.java
@@ -3,6 +3,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+
+// Main area for testing program functionality (not unit tests or integration tests)
 public class App 
 {
     public static void main(String[] args)
@@ -16,6 +18,7 @@ public class App
         Driver driver = new Driver("34@#1234AA", "John", 10, "Heavy", "64|stink|melbourne|victoria|Australia", "10-10-1990");
         DriverRepository repo = new DriverRepository();
         repo.Add(driver);
+        repo.Retrieve("34@#1234AA");
     }
 
 
