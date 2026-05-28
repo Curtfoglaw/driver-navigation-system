52ad69680a4369600bfb8b345b4dfb6a3c09d88e
Curtfoglaw <curtlawrence1@gmail.com>
2026-05-28 13:53:56 +1000

    Added functionality for the 'Update' method

--- driver-navigation/src/main/java/com/rmit/DriverRepository.java
+++ driver-navigation/src/main/java/com/rmit/DriverRepository.java
@@ -3,6 +3,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DriverRepository {
@@ -23,8 +24,52 @@ public class DriverRepository {
         }
     }
 
-    public void Update() {
-        return;
+    public String Update(String driverID, String field, String newValue) {
+        try {
+            myFile = new File("driver-navigation\\src\\DriverStorage.txt");
+            Scanner scnr = new Scanner(myFile);
+            ArrayList<String> lines = new ArrayList<>();
+
+            while (scnr.hasNextLine()) {
+                lines.add(scnr.nextLine());
+            }
+            scnr.close();
+
+            for (int i = 1; i < lines.size(); i++) {
+
+                String[] driverInfo = lines.get(i).split(", ");
+
+                if (driverInfo[0].equals(driverID)) {
+
+                    switch (field) {
+                        case "experienceYears":
+                            driverInfo[2] = newValue;
+                            break;
+                        case "licenseType":
+                            driverInfo[3] = newValue;
+                            break;
+                        case "address":
+                            driverInfo[4] = newValue;
+                            break;
+                        default:
+                            return "Could not change detail. Either user entered invalid detail (ID, name, age) or didn't select valid field";
+                    }
+                    lines.set(i, String.join(", ", driverInfo));
+                    FileWriter writer = new FileWriter("driver-navigation\\src\\DriverStorage.txt");
+
+                    for (String line : lines) {
+                        writer.write(line + "\n");
+                    }
+                    writer.close();
+                    return "Driver updated successfully";
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Driver not found");
+        return null;
+
     }
 
     // Function for retrieving the details of a driver from the .txt file given their ID
@@ -32,7 +77,7 @@ public class DriverRepository {
         try {
             myFile = new File("driver-navigation\\src\\DriverStorage.txt");
             Scanner scnr = new Scanner(myFile);
-
+            scnr.nextLine();
             while (scnr.hasNextLine()) {
 
                 String line = scnr.nextLine();
@@ -56,7 +101,7 @@ public class DriverRepository {
     }
 
     // Function to see how many drivers are currently stored in the .txt file
-    public void Count() {
+    public int Count() {
         int currentCount = 0;
 
         try {
@@ -70,9 +115,11 @@ public class DriverRepository {
             currentCount -= 1;          // Subtract 1 to account for the first line headers
             System.out.println("Number of drivers: " + currentCount);
             scnr.close();
+            return currentCount;
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return 0;
 
     }
 }
