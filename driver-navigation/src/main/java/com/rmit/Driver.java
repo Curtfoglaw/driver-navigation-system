1b83ec4f737ff0407b3d765c8fbfb6fc9ed5a65d
Curtfoglaw <curtlawrence1@gmail.com>
2026-05-31 13:29:00 +1000

    Added another validation method to my driver file

--- driver-navigation/src/main/java/com/rmit/Driver.java
+++ driver-navigation/src/main/java/com/rmit/Driver.java
@@ -1,5 +1,9 @@
 package com.rmit;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 // This is the driver class
 // Drivers will be stored in an array list of other drivers 
 // This array list will act as a way for "driver memory" to persist across .txt files
@@ -56,5 +60,50 @@ public class Driver {
 
     public String getDriverBirthDate() {
         return this.birthdate;
+    }
+
+    // Method for checking if the driver ID follows the basic requirements
+    public static String isValidDriverID(String driverID) {
+        if (driverID.length() != 10) {
+            return "Invalid driver ID. ID must be exactly 10 characters long.";
+        }
+        else if (!Character.isDigit(driverID.charAt(0)) || !Character.isDigit(driverID.charAt(1))) {
+            return "Characters at position 1 and 2 in the ID must be numbers between 1 and 2.";
+        }
+        else if (driverID.charAt(0) < '2' || driverID.charAt(1) < '2') {
+            return "Characters at position 1 and 2 in the ID must be numbers between 2 and 9.";
+        }
+        else if (!Character.isUpperCase(driverID.charAt(8)) || !Character.isUpperCase(driverID.charAt(9))) {
+            return "The last 2 characters of the ID must be upepr case letters.";
+        }
+        else {
+            int specialCharacterCount = 0;
+            for (int i = 2; i < 8; i++) {
+                if (!Character.isLetterOrDigit(driverID.charAt(i))) {
+                    specialCharacterCount += 1;
+                }
+            }
+            if (specialCharacterCount < 2) {
+                return "There must be at least 2 special characters between character 3 and character 8 in the driver ID.";
+            }
+        }
+        return "Driver ID is valid";
+    }
+
+    // Method for checking if the driver address is the correct format
+
+    public static String isValidDriverAddress(String address) {
+        String[] addressParts = address.split("\\|");
+
+        if (addressParts.length != 5) {
+            return "Invalid driver address, incorrect formatting.";
+        }
+        for (String part : addressParts) {
+            if (part.trim().isEmpty()) {
+                return "Invalid driver address, incorrect formatting.";
+            }
+        }
+        return "Valid formatting for driver address.";
+        
     }
 }
