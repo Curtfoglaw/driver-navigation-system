645d8e5bedc075eb1312bc954e4e05c1104cb2c1
Curtfoglaw <curtlawrence1@gmail.com>
2026-05-31 18:48:27 +1000

    First integration test

--- driver-navigation/src/test/java/com/rmit/DriverIntegrationTests.java
+++ driver-navigation/src/test/java/com/rmit/DriverIntegrationTests.java
@@ -6,4 +6,21 @@ import org.junit.jupiter.api.Test;
 
 public class DriverIntegrationTests {
 
+
+    // Integration test to check that a valid driver is stored correctly
+    @Test
+    public void TestDIT1() {
+        
+        DriverRepository repo = new DriverRepository();
+        Driver driver = new Driver("34@#1234AB", "John", 5, "Medium", "12|Main St|Melbourne|VIC|Australia", "12-05-1990");
+        repo.Add(driver);
+        String result = repo.Retrieve(driver.getDriverID());
+        String expectedResult = driver.getDriverInfo();
+
+        assertEquals(expectedResult, result);
+
+        repo.clearDriverStorage();
+    }   
+
+
 }
