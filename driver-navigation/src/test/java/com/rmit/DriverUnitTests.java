f7d52951bdf441f4388eacf3039339c18d01fbf9
Curtfoglaw <curtlawrence1@gmail.com>
2026-05-31 15:46:20 +1000

    Added unit tests for D3

--- driver-navigation/src/test/java/com/rmit/DriverUnitTests.java
+++ driver-navigation/src/test/java/com/rmit/DriverUnitTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 public class DriverUnitTests {
 
+    // Unit testing for valid driver ID
+
     @Test
     public void testD1_ValidDriverID() {
 
@@ -41,6 +43,86 @@ public class DriverUnitTests {
 
         assertEquals("The last 2 characters of the ID must be upepr case letters.", result);
     }
+
+    // Unit testing for valid address format
+
+    @Test
+    public void testD2_ValidDriverAddress() {
+
+        DriverRepository repo = new DriverRepository();
+
+        Driver driver = new Driver("34@#1234AD", "John", 5, "Medium", "12|Able St|Melbourne|VIC|Australia", "12-05-1990");
+
+        String result = repo.Add(driver);
+
+        assertEquals("Driver added successfully.", result);
+
+    }
+
+    @Test
+    public void testD2_InvalidDriverAddress() {
+        
+        DriverRepository repo = new DriverRepository();
+
+        Driver driver = new Driver("34@#1234AE", "John", 5, "Medium", "12|Able St|Melbourne|VIC|", "12-05-1990");
+
+        String result = repo.Add(driver);
+
+        assertEquals("Invalid driver address, incorrect formatting.", result);
+    }
+
+    @Test
+    public void testD2_edgeCase() {
+        
+        DriverRepository repo = new DriverRepository();
+
+        Driver driver = new Driver("34@#1234AF", "John", 5, "Medium", "12, Able St, Melbourne, VIC, Australia", "12-05-1990");
+
+        String result = repo.Add(driver);
+
+        assertEquals("Invalid driver address, incorrect formatting.", result);
+    }
+
+    // Unit testing for valid birthdate format
+
+    @Test
+    public void testD3_ValidBirthdateFormat() {
+        
+        DriverRepository repo = new DriverRepository();
+
+        Driver driver = new Driver("34@#1234AG", "John", 5, "Medium", "12|Able St|Melbourne|VIC|Australia", "12-05-1990");
+
+        String result = repo.Add(driver);
+
+        assertEquals("Driver added successfully.", result);
+    }
+
+    @Test
+    public void testD3_InvalidBirthdateFormat() {
+        
+        DriverRepository repo = new DriverRepository();
+
+        Driver driver = new Driver("34@#1234AH", "John", 5, "Medium", "12|Able St|Melbourne|VIC|Australia", "12-05-");
+
+        String result = repo.Add(driver);
+
+        assertEquals("Invalid driver birthdate, incorrect formatting.", result);
+    }
+
+    @Test
+    public void testD3_edgeCase() {
+        
+        DriverRepository repo = new DriverRepository();
+
+        Driver driver = new Driver("34@#1234AI", "John", 5, "Medium", "12|Able St|Melbourne|VIC|Australia", "12/05/1990");
+
+        String result = repo.Add(driver);
+
+        assertEquals("Invalid driver birthdate, incorrect formatting.", result);
+    }
+
+
+
 
 
 
