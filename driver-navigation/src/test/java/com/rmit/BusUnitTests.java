9923275e2d851ba6bd49a4721a64eb5e5d476a9c
Tedward Leah <s4173080@student.rmit.edu.au>
2026-06-03 22:53:17 +1000

    Added more unit tests

--- driver-navigation/src/test/java/com/rmit/BusUnitTests.java
+++ driver-navigation/src/test/java/com/rmit/BusUnitTests.java
@@ -11,7 +11,7 @@ public class BusUnitTests {
     public void testB1_validID() {
         BusRepository repo = new BusRepository();
 
-        Bus bus = new Bus("12345678", 94, 56.0, "Diesel");
+        Bus bus = new Bus("94826658", 94, 56.0, "Diesel");
 
         String result = repo.Add(bus);
 
@@ -20,6 +20,7 @@ public class BusUnitTests {
         repo.clearBusStorage();
     }
 
+    // Unit testing for duplicate bus ID
     @Test
     public void testB1_duplicateID() {
         BusRepository repo = new BusRepository();
@@ -33,6 +34,96 @@ public class BusUnitTests {
         String result = repo.Add(newBus);
 
         assertEquals("Bus ID must be unique.", result);
+
+        repo.clearBusStorage();
+    }
+
+    // Unit testing for ID above character limit
+    @Test
+    public void testB1_longID() {
+        BusRepository repo = new BusRepository();
+
+        Bus bus = new Bus("123456789", 94, 56.0, "Diesel");
+
+        String result = repo.Add(bus);
+
+        assertEquals("Invalid Bus ID, ID must be exactly 8 characters long.", result);
+
+        repo.clearBusStorage();
+    }
+
+    // Unit testing for ID below character minimum
+    @Test
+    public void testB1_shortID() {
+        BusRepository repo = new BusRepository();
+
+        Bus bus = new Bus("1234567", 94, 56.0, "Diesel");
+
+        String result = repo.Add(bus);
+
+        assertEquals("Invalid Bus ID, ID must be exactly 8 characters long.", result);
+
+        repo.clearBusStorage();
+    }
+
+    // Unit testing for ID with non-numerical characters
+    @Test
+    public void testB1_letterID() {
+        BusRepository repo = new BusRepository();
+
+        Bus bus = new Bus("1fa0tm24", 94, 56.0, "Diesel");
+
+        String result = repo.Add(bus);
+
+        assertEquals("Invalid Bus ID, ID must only contain numerical characters.", result);
+
+        repo.clearBusStorage();
+    }
+
+    // Unit testing for bus update with same capacity
+    @Test 
+    public void testB2_sameCapacity() {
+        BusRepository repo = new BusRepository();
+
+        Bus bus = new Bus("12345678", 100, 56.0, "Diesel");
+
+        repo.Add(bus);
+
+        String result = repo.Update("12345678", "capacity", "100");
+
+        assertEquals("Bus updated successfully", result);
+
+        repo.clearBusStorage();
+    }
+
+    // Unit test for bus update with less capacity
+    @Test 
+    public void testB2_lessCapacity() {
+        BusRepository repo = new BusRepository();
+
+        Bus bus = new Bus("12345678", 100, 56.0, "Diesel");
+
+        repo.Add(bus);
+
+        String result = repo.Update("12345678", "capacity", "80");
+
+        assertEquals("Bus updated successfully", result);
+
+        repo.clearBusStorage();
+    }
+
+    // Unit test for bus update with greater capacity
+    @Test 
+    public void testB2_greaterCapacity() {
+        BusRepository repo = new BusRepository();
+
+        Bus bus = new Bus("12345678", 100, 56.0, "Diesel");
+
+        repo.Add(bus);
+
+        String result = repo.Update("12345678", "capacity", "110");
+
+        assertEquals("Invalid bus capacity, capacity must be less than or equal to previous capacity", result);
 
         repo.clearBusStorage();
     }
